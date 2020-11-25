6a05afb5caa467293501a9ec361563ac8c6cc5ca
alex beeken <alexb@mammothhr.com>
2020-11-25 09:03:16 -0800

    add rulers array override

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,6 +1,14 @@
 require "rulers/version"
+require "rulers/array"
 
 module Rulers
+  class Application
+    def call(env)
+      `echo debug > debug.txt`
+      [200, {'Content-Type' => 'text/html'},
+        ["Hello from ruby on rulers!"]]
+    end
+  end
   class Error < StandardError; end
   # Your code goes here...
 end
